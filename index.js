ab43233f4db868682b5e295d89ef696eea2b1a17
Lee Yang <ny2514@gmail.com>
2020-04-13 14:14:46 -0400

    updated socket.js file

--- index.js
+++ index.js
@@ -1,82 +1,12 @@
 const path = require('path');
-const http = require('http');
 const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
-const {
-  userJoin,
-  getCurrentUser,
-  userLeave,
-  getRoomUsers,
-} = require('./utils/users');
+const socket = require('./utils/socket');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+const app = socket.getExpress();
+const server = socket.getServer();
 
 //set static
 app.use(express.static(path.join(__dirname, 'public')));
-
-const botName = 'WeChat Bot';
-
-//run when a client connects
-io.on('connection', (socket) => {
-  socket.on('joinRoom', ({ username, room }) => {
-    //create a user with id, username, and room
-    const user = userJoin(socket.id, username, room);
-
-    socket.join(user.room);
-
-    //send to client
-    //send welcome message to a connecting user
-    socket.emit('message', formatMessage(botName, 'Welcome to WeChat!'));
-
-    //send msg to client
-    //broadcast to the room user has joined
-    socket.broadcast
-      .to(user.room)
-      .emit(
-        'message',
-        formatMessage(botName, `User ${username} has joined the chat.`)
-      );
-
-    //send users and room info
-    io.to(user.room).emit('roomUsers', {
-      room: user.room,
-      users: getRoomUsers(user.room),
-    });
-  });
-
-  //listen for msg from client
-  socket.on('chatMessage', (msg) => {
-    //get user
-    const user = getCurrentUser(socket.id);
-
-    //relay client msg to user room displaying user
-    io.to(user.room).emit('message', formatMessage(user.username, msg));
-  });
-
-  //runs when client disconnects
-  socket.on('disconnect', () => {
-    //get user from array
-    const user = userLeave(socket.id);
-
-    //if user exists
-    if (user) {
-      //relay msg to user room that users left
-      io.to(user.room).emit(
-        'message',
-        formatMessage(botName, `${user.username} has left the chat`)
-      );
-
-      //send users and room info
-      io.to(user.room).emit('roomUsers', {
-        room: user.room,
-        users: getRoomUsers(user.room),
-      });
-    }
-  });
-});
 
 //listen to port
 const port = process.env.port || 3000;
--- utils/socket.js
+++ utils/socket.js
@@ -1,11 +1,65 @@
+const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const formatMessage = require('./messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./users');
 
-//get express app
-const server = http.createServer();
-
+const app = express();
+const server = http.createServer(app);
 const io = socketio(server);
 
-function getApp(app) {
+const botName = 'WeChat Bot';
+
+io.on('connection', (socket) => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+    socket.emit('message', formatMessage(botName, 'Welcome to WeChat'));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${username} has joined the chat.`)
+      );
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  socket.on('chatMessage', (msg) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+module.exports.getExpress = () => {
   return app;
-}
+};
+
+module.exports.getServer = () => {
+  return server;
+};
+
+module.exports.getExpressStatic = () => {
+  return express.static();
+};
